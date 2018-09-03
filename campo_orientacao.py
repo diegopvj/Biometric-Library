c12405036aa897cb97613256bb002d494781cb8e
DESKTOP-56C2CL2\Diego <diegopvjz@gmail.com>
2018-09-03 01:14:36 -0300

    [Adicionando_Metodos] Mais alguns ajustes de code-style, trocando "" por ''; Adicionando chamada para grandient_orientation passando a imagem convertida e o tamanho do bloco em campo_orientacao.py; Tentativa de implementação da função gradient_orientation que tentará extrair a orientação dos angulos da imagem através do operador de sobel; Adicionando pequenas funções de auxilio para a função gradient_orientation;

--- campo_orientacao.py
+++ campo_orientacao.py
@@ -1,13 +1,16 @@
 import utils
 import options
 
-options = options.passingArg("image")
+options = options.passingArg('image')
 
 im = utils.open_image(options.image[0])
 blockSize = int(options.block_size[0])
 imageConverted = utils.convert_to_black_and_white(im)
+
 showImage = utils.show_image(imageConverted)
 
-if options.name != "":
+gradientOrientation = utils.gradient_orientation(imageConverted, blockSize)
+
+if options.name != '':
     imgOut = options.name
     saveImage = utils.save_image(imageConverted, imgOut)
--- options.py
+++ options.py
@@ -3,9 +3,9 @@
 import argparse
 
 def passingArg(image):
-    parser = argparse.ArgumentParser(description="Campo de Orientação")
-    parser.add_argument(image, nargs=1, help = "Path da imagem")
-    parser.add_argument("block_size", nargs=1, help = "Tamanho do bloco")
-    parser.add_argument('-s', action = "store", dest = 'name', default = "", required = False, help = "Salvar a imagem")
+    parser = argparse.ArgumentParser(description='Campo de Orientação')
+    parser.add_argument(image, nargs=1, help = 'Path da imagem')
+    parser.add_argument('block_size', nargs=1, help = 'Tamanho do bloco')
+    parser.add_argument('-s', action = 'store', dest = 'name', default = '', required = False, help = 'Salvar a imagem')
     args = parser.parse_args()
     return args
