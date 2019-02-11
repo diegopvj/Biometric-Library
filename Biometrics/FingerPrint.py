b3ed4c563baae0d2d97f5111d0ee838b826914e9
Diego Juarez <biometria.uerj@gmail.com>
2019-02-11 15:00:23 -0200

    [Gabor] Adicionando chamada para GaborFilter em FingerPrinty.py

--- Biometrics/FingerPrint.py
+++ Biometrics/FingerPrint.py
@@ -1,20 +1,20 @@
 # encoding: utf-8
 
-from Methods import OrientationField, SmoothOrientationField, Normalization, RidgeFrequency, Segmentation
+from Methods import OrientationField, SmoothOrientationField, Normalization, RidgeFrequency, Segmentation, GaborFilter
 
 def orientation_field_extraction(inputBlockSize):
     print "orientation_field"
-    image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
-    # image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
+    # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
+    image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
     return OrientationField.principal(image, inputBlockSize)
 
-def smooth_orientation_field(inputBlockSize, gaussKernelSize, gaussSigma):
+def smooth_orientation_field(inputBlockSize):
     print "smooth_orientation_field"
-    image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
-    # image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
+    # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
+    image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return SmoothOrientationField.principal(image, inputBlockSize, gaussKernelSize, gaussSigma)
+    return SmoothOrientationField.principal(image, inputBlockSize)
 
 def normalization(inputMean, inputVariance):
     print "normalization"
@@ -22,12 +22,12 @@ def normalization(inputMean, inputVariance):
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     return Normalization.principal(image, inputMean, inputVariance)
 
-def ridge_frequency_extraction(inputBlockSize, gaussKernelSize, gaussSigma):
+def ridge_frequency_extraction(inputBlockSize):
     print "ridge_frequency_extraction"
-    image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
-    # image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
+    # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
+    image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return RidgeFrequency.principal(image, inputBlockSize, gaussKernelSize, gaussSigma)
+    return RidgeFrequency.principal(image, inputBlockSize)
 
 def segmentation(inputBlockSize, inputTreshold):
     print "segmentation"
@@ -35,3 +35,10 @@ def segmentation(inputBlockSize, inputTreshold):
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
     return Segmentation.principal(image, inputBlockSize, inputTreshold)
+
+def gabor_filter(inputBlockSize, xSigma, ySigma):
+    print "gabor_filter"
+    # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
+    image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
+    
+    return GaborFilter.principal(image, inputBlockSize, xSigma, ySigma)
