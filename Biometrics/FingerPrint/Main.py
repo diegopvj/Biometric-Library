ce9e0ee4c88302344431efa6a838d1a45bbc5376
Diego Juarez <biometria.uerj@gmail.com>
2019-02-16 20:07:01 -0200

    [Arquitetura] Alterações finais para que a biblioteca faça sentido de acordo com o projeto final;

--- Biometrics/FingerPrint/Main.py
+++ Biometrics/FingerPrint/Main.py
@@ -7,41 +7,41 @@ def orientation_field_extraction(inputBlockSize):
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return OrientationField.principal(image, inputBlockSize)
+    return OrientationField.main(image, inputBlockSize)
 
 def smooth_orientation_field(inputBlockSize):
     print "smooth_orientation_field"
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return SmoothOrientationField.principal(image, inputBlockSize)
+    return SmoothOrientationField.main(image, inputBlockSize)
 
 def normalization(inputMean, inputVariance):
     print "normalization"
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
-    return Normalization.principal(image, inputMean, inputVariance)
+    return Normalization.main(image, inputMean, inputVariance)
 
 def ridge_frequency_extraction(inputBlockSize):
     print "ridge_frequency_extraction"
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return RidgeFrequency.principal(image, inputBlockSize)
+    return RidgeFrequency.main(image, inputBlockSize)
 
 def segmentation(inputBlockSize, inputTreshold):
     print "segmentation"
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return Segmentation.principal(image, inputBlockSize, inputTreshold)
+    return Segmentation.main(image, inputBlockSize, inputTreshold)
 
 def gabor_filter(inputBlockSize, xSigma, ySigma):
     print "gabor_filter"
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return GaborFilter.principal(image, inputBlockSize, xSigma, ySigma)
+    return GaborFilter.main(image, inputBlockSize, xSigma, ySigma)
 
 def thinning_structures():
     print "gabor_filter"
@@ -49,4 +49,4 @@ def thinning_structures():
     # image = "C:\\Users\\Diego\\Documents\\biometrics-master\\images\\ppf1.png"
     # image = "C:\\Users\\Diego\\Desktop\\digitais\\8_3.tif"
     
-    return Thinning.principal(image)
+    return Thinning.main(image)
--- Biometrics/FingerPrint/Methods/Segmentation.py
+++ Biometrics/FingerPrint/Methods/Segmentation.py
@@ -6,13 +6,13 @@ import itertools
 def main(image, inputBlockSize, threshold):
     originalImage = Utils.open_image(image)
     imageConverted = Utils.convert_to_black_and_white(originalImage)
-    segmentedImage, varianceImage = segmented_and_variance_impl(imageConverted, inputBlockSize, threshold)
+    segmentedImage, varianceImage = segmentation_and_variance_impl(imageConverted, inputBlockSize, threshold)
     Utils.show_image(segmentedImage)
     Utils.show_image(varianceImage)
     return segmentedImage, varianceImage
 
 
-def segmented_and_variance_impl(image, inputBlockSize, threshold):
+def segmentation_and_variance_impl(image, inputBlockSize, threshold):
     imageSize = Utils.get_size(image)
     baseSegmentedImage = Utils.image_copy(image)
     baseVarianceImage = Utils.image_copy(image)
